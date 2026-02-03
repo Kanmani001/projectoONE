189fda7137ec2543b98ff2d3440119a3443fd2d6
Kanmani001 <kanmani0516@gmail.com>
2026-02-03 12:49:46 +0800

    chaged url

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -2,4 +2,4 @@ const isLocal = window.location.hostname.includes("localhost");
 
 export const API_BASE = isLocal
   ? "http://localhost:3000/api"
-  : "https://projecto-ff6nsdep-kannani001s-projects.vercel.app/api";
+  : "https://projecto-n8938gg5n-kanmani001s-projects.vercel.app/api";
